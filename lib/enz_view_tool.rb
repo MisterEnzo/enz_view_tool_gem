3952dfab09fd2ded28484c3e97bc22bb8e28bc40
Enzo Naguiat <enzo_naguiat@yahoo.com>
2018-09-29 17:23:15 +0800

    initial commit

---
+++ lib/enz_view_tool.rb
@@ -0,0 +1,6 @@
+require "enz_view_tool/version"
+require "enz_view_tool/renderer"
+
+module EnzViewTool
+  # Your code goes here...
+end
---
+++ lib/enz_view_tool/version.rb
@@ -0,0 +1,3 @@
+module EnzViewTool
+  VERSION = "0.1.0"
+end
